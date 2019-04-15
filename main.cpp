d19a2a4605b250fe8bfe9e9c83b17fe9d49779e7
kentaro <kentarolinux@gmail.com>
2019-04-16 01:30:53 +0900

    output ppm (p1)

--- main.cpp
+++ main.cpp
@@ -5,12 +5,19 @@
 
 int
 main () {
-  auto interval = 0.1L;
+  auto interval = 0.0005L;
   hasokon::mandelbrot::mandelbrot_set set{ interval };
+
+  std::cout << "P1" << std::endl;
+  std::cout << static_cast<unsigned long int>((2.0L / interval) * 2);
+  std::cout << " ";
+  std::cout << static_cast<unsigned long int>((2.0L / interval) * 2);
+  std::cout << std::endl;
+
   for (long double x = -2.0L; x <= 2.0L; x += interval) {
     for (long double y = -2.0L; y <= 2.0L; y += interval) {
       std::cout << set.get(x,y);
-      std::cout << set.get(x,y);
+      std::cout << " ";
     }
     std::cout << std::endl;
   }
