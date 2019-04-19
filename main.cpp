1be9f831ec5a5c1132c51e3349cb22f57c72aeb3
kentaro <kentarolinux@gmail.com>
2019-04-20 02:51:05 +0900

    メモリ効率あげた

--- main.cpp
+++ main.cpp
@@ -5,18 +5,21 @@
 
 int
 main () {
-  auto interval = 0.0005L;
-  hasokon::mandelbrot::mandelbrot_set set{ interval };
+  auto sx = 0.0L;
+  auto sy = 0.7L;
+  auto len = 0.4L;
+  auto interval = 0.0001L;
+  hasokon::mandelbrot::mandelbrot_set set{ sx, sy, len, interval };
 
   std::cout << "P1" << std::endl;
-  std::cout << static_cast<unsigned long int>((2.0L / interval) * 2);
+  std::cout << set.getRelativeWidth();
   std::cout << " ";
-  std::cout << static_cast<unsigned long int>((2.0L / interval) * 2);
+  std::cout << set.getRelativeHeight();
   std::cout << std::endl;
 
-  for (long double x = -2.0L; x <= 2.0L; x += interval) {
-    for (long double y = -2.0L; y <= 2.0L; y += interval) {
-      std::cout << set.get(x,y);
+  for (uint32_t y = 0; y < set.getRelativeHeight(); ++y) {
+    for (uint32_t x = 0; x < set.getRelativeWidth(); ++x) {
+      std::cout << set.getByRelativePoint(x,y);
       std::cout << " ";
     }
     std::cout << std::endl;
--- mandelbrot_set.h
+++ mandelbrot_set.h
@@ -5,7 +5,6 @@
 #include <complex>
 #include <cstdint>
 #include <vector>
-#include <map>
 #include <cstddef>
 
 namespace hasokon {
@@ -19,9 +18,10 @@ template <typename FloatT>
 bool include_in_mandelbrot_set(FloatT const x, FloatT const y, uint32_t const calc_size = DEFAULT_CALCSIZE) {
   std::complex const c { x, y };
   std::complex z{ static_cast<FloatT>(0.0), static_cast<FloatT>(0.0) };
+  auto boundary = static_cast<FloatT>(BOUNDARY);
   uint32_t i = calc_size;
   while (i--) {
-    if (std::abs(z) > static_cast<FloatT>(BOUNDARY)) return false;
+    if (std::abs(z) > boundary) return false;
     z = z * z + c;
   }
   return true;
@@ -30,25 +30,67 @@ bool include_in_mandelbrot_set(FloatT const x, FloatT const y, uint32_t const ca
 template <typename FloatT>
 class mandelbrot_set {
 private:
-  std::map<FloatT, std::map<FloatT, bool>> _set{};
+  std::vector<bool> _data;
   FloatT _interval;
+  FloatT _startPointX;
+  FloatT _startPointY;
+  FloatT _width;
+  FloatT _height;
+  uint32_t _relativeWidth;
+  uint32_t _relativeHeight;
 public:
-  constexpr mandelbrot_set (FloatT const interval) noexcept : _interval(interval) {
-    FloatT const boundary = static_cast<FloatT>(BOUNDARY);
-    uint32_t const size = static_cast<uint32_t>( boundary / interval);
-    for (FloatT y = -1 * boundary; y <= boundary; y += interval) {
-      _set[y] = std::map<FloatT, bool>{};
-      for (FloatT x = -1 * boundary; x <= boundary; x += interval) {
-        _set[y][x] = include_in_mandelbrot_set(x, y);
+  mandelbrot_set (FloatT const sx, FloatT const sy, FloatT w, FloatT h, FloatT const interval) :
+  _interval(interval),
+  _startPointX(sx),
+  _startPointY(sy),
+  _width(w),
+  _height(h),
+  _relativeWidth(static_cast<uint32_t>(w/interval)),
+  _relativeHeight(static_cast<uint32_t>(h/interval)),
+  _data(static_cast<size_t>(_relativeWidth * _relativeHeight))
+  {
+    _data.clear();
+    for (FloatT y = sy; y >= sy - h; y -= interval) {
+      for (FloatT x = sx; x <= sx + w; x += interval) {
+        _data.push_back(include_in_mandelbrot_set(x, y));
       }
     }
   }
 
-  bool const get(FloatT const x, FloatT const y) const {
-    return _set.at(x).at(y);
+  mandelbrot_set(FloatT const sx, FloatT const sy, FloatT const len, FloatT const interval) : mandelbrot_set(sx, sy, len, len, interval) {}
+  mandelbrot_set(FloatT const s, FloatT const len, FloatT const interval) : mandelbrot_set(s, s, len, interval) {}
+  mandelbrot_set(FloatT const interval) : mandelbrot_set(-1 * static_cast<FloatT>(BOUNDARY), static_cast<FloatT>(BOUNDARY * 2), interval) {}
+
+  bool const getByRelativePoint(uint32_t const x, uint32_t const y) const {
+    return _data.at(y * _relativeWidth + x);
   }
 
-  void export_file(std::string const filename) const {
+  FloatT const getInterval() const noexcept {
+    return _interval;
+  }
+
+  FloatT const getStartPointX() const noexcept {
+    return _startPointX;
+  }
+
+  FloatT const getStartPointY() const noexcept {
+    return _startPointY;
+  }
+
+  FloatT getWidth() const noexcept {
+    return _width;
+  }
+
+  FloatT getHeight() const noexcept {
+    return _height;
+  }
+
+  uint32_t getRelativeWidth() const noexcept {
+    return _relativeWidth;
+  }
+
+  uint32_t getRelativeHeight() const noexcept {
+    return _relativeHeight;
   }
 };
 
